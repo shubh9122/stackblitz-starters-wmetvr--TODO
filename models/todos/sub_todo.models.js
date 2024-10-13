cee87ceb27a27f5d83cea3baf494054e2626bfd5
shubh9122 <git@stackblitz.com>
2024-10-13 10:53:17 +0530

    Initial commit

---
+++ models/todos/sub_todo.models.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+);
+
+export const SubTodo = mongoose.model('SubTodo', subTodoSchema);
+
+// import mongoose from 'mongoose';
+
+// const subTodoSchema = new mongoose.Schema(
+//   {
+//     content: {
+//       type: String,
+//       required: true,
+//     },
+//     complete: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     createdBy: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'User',
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export const SubTodo = mongoose.model('SubTodo', subTodoSchema);
---
+++ models/todos/user.models.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+import { type } from 'os';
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model('User', userSchema);
+
+// import mongoose from 'mongoose';
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export const User = mongoose.model('User', userSchema);
